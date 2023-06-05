17a53665bacead5e1aa43dcc95cb994c4c8a1db0
Reba Magier <magier.rebecca.12@gmail.com>
2023-06-05 15:19:17 -0400

    Update message_notification.py

--- canvas_workflow_helpers/protocols/message_notification.py
+++ canvas_workflow_helpers/protocols/message_notification.py
@@ -1,62 +1,76 @@
 import json
 
-from canvas_workflow_kit import events
 from canvas_workflow_kit.constants import CHANGE_TYPE
 from canvas_workflow_kit.protocol import (STATUS_NOT_APPLICABLE,
                                           ClinicalQualityMeasure,
                                           ProtocolResult)
 from canvas_workflow_kit.utils import send_notification
+import requests
 
 
 class MessageNotification(ClinicalQualityMeasure):
-    """
-    Protocol that listens for messages and sends a notification once the 'send' button is clicked in the UI.
-    """
-
     class Meta:
-
         title = 'Message Notification'
-
         version = 'v0.0.1'
-
         description = 'Listens for message submission and sends a notification.'
-
-        information = 'https://canvasmedical.com/'
-
-        identifiers = ['MessageNotification']
-
         types = ['Notification']
-
-        responds_to_event_types = [
-            events.HEALTH_MAINTENANCE,
-        ]
-
         compute_on_change_types = [CHANGE_TYPE.MESSAGE]
-
-        authors = ['Canvas Medical']
-
-        references = ['Canvas Medical']
-
-        funding_source = ''
-
         notification_only = True
 
-    # REPLACE this url with your server url which should receive these notifications
-    notification_url = 'https://webhook.site/360e0615-d7c0-430a-9b11-7d781390da36'
-    headers = {'Content-Type': 'application/json'}
+    def get_fhir_api_token(self):
+        """ Given the Client ID and Client Secret for authentication to FHIR,
+        return a bearer token """
+
+        grant_type = "client_credentials"
+        client_id = self.settings.CLIENT_ID
+        client_secret = self.settings.CLIENT_SECRET
+
+        token_response = requests.request(
+            "POST",
+            f'https://{self.instance_name}.canvasmedical.com/auth/token/',
+            headers={'Content-Type': 'application/x-www-form-urlencoded'},
+            data=f"grant_type={grant_type}&client_id={client_id}&client_secret={client_secret}"
+        )
+
+        if token_response.status_code != 200:
+            raise Exception('Unable to get a valid FHIR bearer token')
+
+        return token_response.json().get('access_token')
+
+    def get_fhir_communication(self, message_id):
+        """ Given a Task ID we can perform a FHIR Task Search Request"""
+        request = (f"https://fhir-{self.instance_name}.canvasmedical.com/"
+             f"Communication?_id={message_id}")
+        response = requests.get(
+            request,
+            headers={
+                'Authorization': f'Bearer {self.get_fhir_api_token()}',
+                'accept': 'application/json'
+            })
+
+        if response.status_code != 200:
+            raise Exception(f"Failed to retrieve FHIR Communicaton with {request}")
+
+        return response.json()['entry'][0]
 
     def compute_results(self):
         result = ProtocolResult()
         result.status = STATUS_NOT_APPLICABLE
 
-
         if self.field_changes['model_name'] == 'messagetransmission':
+            self.instance_name = self.settings.INSTANCE_NAME
+            self.token = self.get_fhir_api_token()
+
             canvas_id = self.field_changes.get('fields', {}).get('message_id', None)
             if canvas_id:
-                self.base_payload = {
-                    'canvas_patient_key': self.patient.patient['key'],
-                    'message_info': self.patient.messages.filter(id=canvas_id[1]).records
-                }
-                send_notification(self.notification_url, json.dumps(self.base_payload), self.headers)
+                message = self.patient.messages.filter(id=canvas_id[1]).records
+                if message:
+                    message_id = message[-1]['externallyExposableId']
+                    
+                    # TODO change the url for your needs
+                    send_notification(
+                        'https://webhook.site/0a8f7cb1-fcc5-421c-8a99-9c87533cf678',
+                        json.dumps(self.get_fhir_communication(message_id)),
+                        headers={'Content-Type': 'application/json'})
 
         return result
